fbea024465d4c51a969cfdacfd58fee7cf2c8007
Daniel-Alves-Dev-777 <r.bellini@aluno.ifsp.edu.br>
2025-10-15 10:42:08 -0300

    mudanças 15-10

--- aula07/settings.py
+++ aula07/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-c4!uz0hwdmkk^p^q3!3g_x$b#7+&7_lv(j)0zvc3v92vztyml4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dsw2025-aula-09.onrender.com']
+ALLOWED_HOSTS = ['dsw2025-aula-09.onrender.com', '127.0.0.1']
 
 
 # Application definition
